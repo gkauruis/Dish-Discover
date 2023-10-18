8747a0ddcf862f5f838b58b306c76f777c5f4d42
Procerus <lmd742@hotmail.com>
2023-10-17 22:45:41 -0500

    remove old git files, add json file and assets folder, created new class files for recipe object

--- app/src/main/java/com/example/dishdiscover/MainActivity.java
+++ app/src/main/java/com/example/dishdiscover/MainActivity.java
@@ -5,9 +5,17 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,11 +27,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        String jsonString;
+        JSONObject reciperaw;
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        StringBuilder stringBuilder;
+        try {
+            AssetManager assetFiles = getAssets();
+            String[] files = assetFiles.list("");
+            InputStream inputStream = getAssets().open("recipes.json");
 
-        recipes.add(new Recipe("Crepes", R.drawable.crepes));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        jsonString = stringBuilder.toString();
+        try {
+            reciperaw = new JSONObject(jsonString).getJSONObject("Recipe");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            recipes.add(new Recipe(reciperaw.getJSONObject("Mealfacts").getString("MealName"), R.drawable.crepes));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
         recipes.add(new Recipe("Dosa", R.drawable.dosa));
 
         recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
--- app/src/main/java/com/example/dishdiscover/Recipe.java
+++ app/src/main/java/com/example/dishdiscover/Recipe.java
@@ -1,10 +1,16 @@
 package com.example.dishdiscover;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Recipe {
 
     String recipeName;
     int recipeImage;
+    List<Ingredient> ingredients= new ArrayList<Ingredient>();
+    List<String> category = new ArrayList<String>();
+    Nutrition nutrition = null;
 
+    MealFacts facts = null;
+    List<List<String>> steps = new ArrayList<List<String>>();
     public Recipe(String recipeName, int recipeImage) {
         this.recipeName = recipeName;
         this.recipeImage = recipeImage;
@@ -12,6 +18,12 @@ public class Recipe {
 
     public String getRecipeName() {
         return recipeName;
+    }
+
+    public void add_ingredient(String name,int amt,String unit)
+    {
+        Ingredient ing = new Ingredient(name,amt,unit);
+        this.ingredients.add(ing);
     }
 
     public void setRecipeName(String recipeName) {
@@ -24,5 +36,63 @@ public class Recipe {
 
     public void setRecipeImage(int recipeImage) {
         this.recipeImage = recipeImage;
+    }
+}
+
+class Ingredient {
+    String name;
+    double amt;
+    String unit;
+
+    public String getAmount() {return amt + " " + unit;}
+
+    public Ingredient(String name, double amt, String unit){
+        this.name = name;
+        this.amt = amt;
+        this.unit = unit;
+    }
+}
+
+class Nutrition {
+    double cal;
+    double fat;
+    double sugar;
+    double salt;
+
+    public Nutrition(double cal, double fat, double sugar, double salt){
+        this.cal = cal;
+        this.fat = fat;
+        this.sugar = sugar;
+        this.salt = salt;
+    }
+}
+
+class MealFacts {
+    String mealName;
+    String mealImage;
+    String mealDescription;
+    String difficulty;
+    double prep;
+    double cooktime;
+    int serves;
+    double rating;
+    String weburl;
+    String comments;
+
+    public MealFacts(String mealName,String mealImage,
+                     String mealDescription, String difficulty,
+                     double prep, double cooktime, int serves,
+                     double rating, String weburl, String comments)
+    {
+        this.mealName = mealName;
+        this.mealImage = mealImage;
+        this.mealDescription = mealDescription;
+        this.difficulty = difficulty;
+        this.prep = prep;
+        this.cooktime = cooktime;
+        this.serves = serves;
+        this.rating = rating;
+        this.weburl = weburl;
+        this.comments = comments;
     }
 }
