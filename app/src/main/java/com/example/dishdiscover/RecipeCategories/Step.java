dcad8e8c522db3709ee4aeb44712da04aa29fe00
Matt <matt_ehlert@outlook.com>
2023-10-23 15:16:36 -0500

    Commit to perform merge

--- app/src/main/java/com/example/dishdiscover/RecipeCategories/Step.java
+++ app/src/main/java/com/example/dishdiscover/RecipeCategories/Step.java
@@ -13,15 +13,45 @@ public class Step {
         this.stepImage = null;
     }
 
+    public Step(String number) {
+        this.number = number;
+        this.action = null;
+        this.stepImage = null;
+    }
+
+    public Step(String number, String action) {
+        this.number = number;
+        this.action = action;
+        this.stepImage = null;
+    }
+
+    public Step(String number, String action, String stepIamge) {
+        this.number = number;
+        this.action = action;
+        this.stepImage = stepIamge;
+    }
+
     public String getNumber() {
         return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
     }
 
     public String getAction() {
         return action;
     }
 
+    public void setAction(String action) {
+        this.action = action;
+    }
+
     public String getStepImage() {
         return stepImage;
+    }
+
+    public void setStepImage(String stepImage) {
+        this.stepImage = stepImage;
     }
 }
